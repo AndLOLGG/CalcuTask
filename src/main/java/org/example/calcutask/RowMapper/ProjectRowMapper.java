014956e4398822c8f5d6122a2a39df7a1b48f8eb
AndLOLGG <andreassilas@gmail.com>
2025-05-02 15:40:44 +0200

    ProjectRowMapper.java

--- src/main/java/org/example/calcutask/RowMapper/ProjectRowMapper.java
+++ src/main/java/org/example/calcutask/RowMapper/ProjectRowMapper.java
@@ -1,4 +1,20 @@
 package org.example.calcutask.RowMapper;
 
-public class ProjectRowMapper {
+import org.example.calcutask.Model.Project;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProjectRowMapper implements RowMapper<Project> {
+    @Override
+    public Project mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Project project = new Project();
+        project.setProjectId(rs.getInt("projectId"));
+        project.setProjectName(rs.getString("projectName"));
+        project.setProjectDescription(rs.getString("projectDescription"));
+        project.setCreatedDate(rs.getDate("createdDate"));
+        project.setCreatedBy(rs.getString("createdBy"));
+        return project;
+    }
 }
