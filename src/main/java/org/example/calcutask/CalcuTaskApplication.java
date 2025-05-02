5e60f0ef39f56da084cec5c34fdd8ed0655a0869
AndLOLGG <andreassilas@gmail.com>
2025-05-02 13:30:38 +0200

    Second commit

---
+++ src/main/java/org/example/calcutask/CalcuTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.calcutask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcuTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcuTaskApplication.class, args);
+	}
+
+}
