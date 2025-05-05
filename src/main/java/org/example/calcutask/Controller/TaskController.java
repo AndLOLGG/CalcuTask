36342c412acc0fbd5505b85757389dc695251b7a
luceedot <asqv0001@stud.kea.dk>
2025-05-05 15:01:13 +0200

    controller work

--- src/main/java/org/example/calcutask/Controller/TaskController.java
+++ src/main/java/org/example/calcutask/Controller/TaskController.java
@@ -1,4 +1,14 @@
 package org.example.calcutask.Controller;
+import org.springframework.stereotype.Controller;
+import org.example.calcutask.Service.TaskService;
 
+@Controller
 public class TaskController {
+    private final TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+
+
 }
--- src/main/java/org/example/calcutask/Controller/UserController.java
+++ src/main/java/org/example/calcutask/Controller/UserController.java
@@ -1,4 +1,11 @@
 package org.example.calcutask.Controller;
+import org.springframework.stereotype.Controller;
+import org.example.calcutask.Service.UserService;
 
+@Controller
 public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
