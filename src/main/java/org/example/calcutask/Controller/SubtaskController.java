dbf00a2d912dddf9763836b47ead455d07738871
luceedot <asqv0001@stud.kea.dk>
2025-05-06 14:19:12 +0200

    controller work + added import

--- src/main/java/org/example/calcutask/Controller/SubtaskController.java
+++ src/main/java/org/example/calcutask/Controller/SubtaskController.java
@@ -1,4 +1,22 @@
 package org.example.calcutask.Controller;
 
+import org.example.calcutask.Service.SubtaskService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SubtaskController {
-}
+
+
+        private final SubtaskService subtaskService;
+        public SubtaskController(SubtaskService subtaskService) {
+            this.subtaskService = subtaskService;
+        }
+
+
+
+    }
+
+
+
+
+
